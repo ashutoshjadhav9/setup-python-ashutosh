c77013c8979b3fa8eba41dc8db9183a564bd0e2f
ashutoshjadhav9 <Ashutosh.Jadhav2@ibm.com>
2023-12-11 14:25:06 +0530

    1

--- dist/setup/index.js
+++ dist/setup/index.js
@@ -11025,7 +11025,6 @@ function getManifestFromRepo(owner, repo, auth, branch = 'master') {
         console.log("=========== in getManifestFromRepo function 4 =============");
         let manifestUrl = '';
         for (const item of response.result.tree) {
-            console.log(" === item.url === " + item.url);
             console.log(" === item.path === " + item.path);
             if (item.path === 'versions-manifest.json') {
                 console.log(" === item.url === " + item.url);
