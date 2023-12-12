817b4be722314528d3539aca166d5f93a21a786a
ashutoshjadhav9 <Ashutosh.Jadhav2@ibm.com>
2023-12-12 10:35:32 +0530

    1

--- dist/setup/index.js
+++ dist/setup/index.js
@@ -70188,8 +70188,8 @@ const MANIFEST_REPO_NAME = 'python-build-standalone';
 const MANIFEST_REPO_BRANCH = 'main';
 console.log(" ========== Ashutosh ========== ");
 exports.MANIFEST_URL = `https://raw.githubusercontent.com/${MANIFEST_REPO_OWNER}/${MANIFEST_REPO_NAME}/${MANIFEST_REPO_BRANCH}/versions-manifest.json`;
-arch = "ppc64le"
-function findReleaseFromManifest(semanticVersionSpec, architecture, manifest) {
+//arch = "ppc64le"
+function findReleaseFromManifest(semanticVersionSpec, "ppc64le", manifest) {
     return __awaiter(this, void 0, void 0, function* () {
         console.log("=========== start =============");
         if (!manifest) {
